a41ced625e77df14d54c2a443b2ab7e5b66fb564
Mikhail Burilov <burilovmv@gmail.com>
2019-08-21 14:02:11 +0700

    Azure Cosmos supply collector and tests

---
+++ AzureCosmosSupplyCollector/AzureCosmosSupplyCollector.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Newtonsoft.Json.Linq;
+using S2.BlackSwan.SupplyCollector;
+using S2.BlackSwan.SupplyCollector.Models;
+using DataType = S2.BlackSwan.SupplyCollector.Models.DataType;
+
+namespace AzureCosmosSupplyCollector {
+    public class AzureCosmosSupplyCollector : SupplyCollectorBase {
+        public override List<string> DataStoreTypes() {
+            return (new[] {"Azure Cosmos"}).ToList();
+        }
+
+        public string BuildConnectionString(string accountEndpoint, string accountKey, string database)
+        {
+            return $"AccountEndpoint={accountEndpoint};AccountKey={accountKey};Database={database}";
+        }
+
+        private void ParseConnectString(string connectString, out string endpoint, out string accountKey,
+            out string database) {
+
+            endpoint = null;
+            accountKey = null;
+            database = null;
+
+            var connectStringParts = connectString.Split(";");
+            foreach (var part in connectStringParts) {
+                if(String.IsNullOrEmpty(part))
+                    continue;
+
+                var index = part.IndexOf("=");
+                if (index > 0) {
+                    var key = part.Substring(0, index);
+                    var val = part.Substring(index + 1);
+
+                    if ("AccountEndpoint".Equals(key, StringComparison.InvariantCultureIgnoreCase)) {
+                        endpoint = val;
+                    }
+                    else if ("AccountKey".Equals(key, StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        accountKey = val;
+                    }
+                    else if ("Database".Equals(key, StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        database = val;
+                    }
+                }
+            }
+        }
+
+        public override List<string> CollectSample(DataEntity dataEntity, int sampleSize) {
+            var result = new List<string>();
+
+            string endpoint;
+            string key;
+            string database;
+            ParseConnectString(dataEntity.Container.ConnectionString, out endpoint, out key, out database);
+
+            using (var client = new DocumentClient(new Uri(endpoint), key))
+            {
+                var db = (Database)client.CreateDatabaseQuery($"SELECT * from d where d.id=\"{database}\"").ToList()
+                    .FirstOrDefault();
+
+                if (db == null)
+                {
+                    throw new ArgumentException($"Database {database} not found!");
+                }
+
+                var list = client.CreateDocumentQuery(
+                    UriFactory.CreateDocumentCollectionUri(database, dataEntity.Collection.Name),
+                    $"SELECT t.{dataEntity.Name} FROM {dataEntity.Collection.Name} t", new FeedOptions() {MaxItemCount = sampleSize});
+
+                int count = 0;
+                foreach (var item in list) {
+                    var obj = JObject.Parse(item.ToString());
+                    
+                    result.Add(obj[dataEntity.Name].ToString());
+                    
+                    if (++count >= sampleSize)
+                        break;
+                }
+            }
+
+            return result;
+        }
+
+        public override List<DataCollectionMetrics> GetDataCollectionMetrics(DataContainer container) {
+            var metrics = new List<DataCollectionMetrics>();
+
+            string endpoint;
+            string key;
+            string database;
+            ParseConnectString(container.ConnectionString, out endpoint, out key, out database);
+
+            using (var client = new DocumentClient(new Uri(endpoint), key))
+            {
+                var db = (Database)client.CreateDatabaseQuery($"SELECT * from d where d.id=\"{database}\"").ToList()
+                    .FirstOrDefault();
+
+                if (db == null)
+                {
+                    throw new ArgumentException($"Database {database} not found!");
+                }
+
+                var docs = client.CreateDocumentCollectionQuery(db.SelfLink).ToArray();
+
+                foreach (var doc in docs)
+                {
+                    var rows = client.CreateDocumentQuery(
+                        UriFactory.CreateDocumentCollectionUri(database, doc.Id),
+                        $"SELECT VALUE COUNT(1) FROM {doc.Id}");
+
+                    foreach (var row in rows) {
+                        metrics.Add(new DataCollectionMetrics()
+                        {
+                            Name = doc.Id,
+                            RowCount = Int64.Parse(row.ToString())
+                        });
+
+                        break;
+                    }
+                }
+            }
+
+            return metrics;
+        }
+
+        private void FillObjectEntities(DataContainer container, DataCollection collection, string prefix, JObject obj, List<DataEntity> entities)
+        {
+            var properties = obj.Properties();
+            foreach (var property in properties)
+            {
+                if (entities.Find(x => x.Name.Equals($"{prefix}{property.Name}")) != null)
+                {
+                    continue;
+                }
+
+                switch (property.Value.Type)
+                {
+                    case JTokenType.Boolean:
+                        entities.Add(new DataEntity($"{prefix}{property.Name}", DataType.Boolean, "Boolean", container, collection));
+                        break;
+                    case JTokenType.String:
+                        entities.Add(new DataEntity($"{prefix}{property.Name}", DataType.String, "String", container, collection));
+                        break;
+                    case JTokenType.Date:
+                        entities.Add(new DataEntity($"{prefix}{property.Name}", DataType.DateTime, "Date", container, collection));
+                        break;
+                    case JTokenType.Float:
+                        entities.Add(new DataEntity($"{prefix}{property.Name}", DataType.Float, "Float", container, collection));
+                        break;
+                    case JTokenType.Integer:
+                        entities.Add(new DataEntity($"{prefix}{property.Name}", DataType.Int, "Integer", container, collection));
+                        break;
+                    case JTokenType.Guid:
+                        entities.Add(new DataEntity($"{prefix}{property.Name}", DataType.Guid, "Guid", container, collection));
+                        break;
+                    case JTokenType.Uri:
+                        entities.Add(new DataEntity($"{prefix}{property.Name}", DataType.String, "Uri", container, collection));
+                        break;
+                    case JTokenType.Array:
+                        entities.Add(new DataEntity($"{prefix}{property.Name}", DataType.String, "Array", container, collection));
+
+                        var arr = (JArray)property.Value;
+                        for (int i = 0; i < arr.Count; i++)
+                        {
+                            var arrayItem = arr[i];
+
+                            if (arrayItem.Type == JTokenType.Object)
+                            {
+                                FillObjectEntities(container, collection, $"{prefix}{property.Name}.", (JObject)arrayItem, entities);
+                            }
+                        }
+
+                        break;
+                    case JTokenType.Object:
+                        FillObjectEntities(container, collection, $"{prefix}{property.Name}.", (JObject)property.Value, entities);
+                        break;
+                    default:
+                        entities.Add(new DataEntity($"{prefix}{property.Name}",
+                            DataType.Unknown, Enum.GetName(typeof(JTokenType), property.Value.Type), container, collection));
+                        break;
+                }
+            }
+        }
+
+        public override (List<DataCollection>, List<DataEntity>) GetSchema(DataContainer container) {
+            var collections = new List<DataCollection>();
+            var entities = new List<DataEntity>();
+
+            string endpoint;
+            string key;
+            string database;
+            ParseConnectString(container.ConnectionString, out endpoint, out key, out database);
+
+            using (var client = new DocumentClient(new Uri(endpoint), key))
+            {
+                var db = (Database)client.CreateDatabaseQuery($"SELECT * from d where d.id=\"{database}\"").ToList()
+                    .FirstOrDefault();
+
+                if (db == null) {
+                    throw new ArgumentException($"Database {database} not found!");
+                }
+
+                var docs = client.CreateDocumentCollectionQuery(db.SelfLink).ToArray();
+
+                foreach (var doc in docs) {
+                    var collection = new DataCollection(container, doc.Id);
+                    collections.Add(collection);
+
+                    var rows = client.CreateDocumentQuery(
+                        UriFactory.CreateDocumentCollectionUri(database, doc.Id),
+                        $"SELECT * FROM {doc.Id}");
+
+                    foreach (var row in rows) {
+                        var obj = JObject.Parse(row.ToString());
+
+                        FillObjectEntities(container, collection, "", obj, entities);
+                        break;
+                    }
+                }
+            }
+
+            return (collections, entities);
+        }
+
+        public override bool TestConnection(DataContainer container) {
+            string endpoint;
+            string key;
+            string database;
+            ParseConnectString(container.ConnectionString, out endpoint, out key, out database);
+
+            try {
+                using (var client = new DocumentClient(new Uri(endpoint), key)) {
+                    var db = (Database)client.CreateDatabaseQuery($"SELECT * from d where d.id=\"{database}\"").ToList()
+                        .FirstOrDefault();
+
+                    return db != null;
+                }
+            }
+            catch (Exception) {
+                return false;
+            }
+        }
+
+
+    }
+}
---
+++ AzureCosmosSupplyCollectorTests/AzureCosmosSupplyCollectorTests.cs
@@ -0,0 +1,93 @@
+using System;
+using S2.BlackSwan.SupplyCollector.Models;
+using Xunit;
+
+namespace AzureCosmosSupplyCollectorTests
+{
+    public class AzureCosmosSupplyCollectorTests : IClassFixture<LaunchSettingsFixture>
+    {
+        private readonly AzureCosmosSupplyCollector.AzureCosmosSupplyCollector _instance;
+        public readonly DataContainer _container;
+        private LaunchSettingsFixture _fixture;
+
+        public AzureCosmosSupplyCollectorTests(LaunchSettingsFixture fixture)
+        {
+            _fixture = fixture;
+            _instance = new AzureCosmosSupplyCollector.AzureCosmosSupplyCollector();
+            _container = new DataContainer()
+            {
+                ConnectionString = _instance.BuildConnectionString(
+                    Environment.GetEnvironmentVariable("COSMOS_DB_URI"),
+                    Environment.GetEnvironmentVariable("COSMOS_DB_KEY"),
+                    Environment.GetEnvironmentVariable("COSMOS_DB_DATABASE")
+                )
+            };
+        }
+
+        [Fact]
+        public void DataStoreTypesTest()
+        {
+            var result = _instance.DataStoreTypes();
+            Assert.Contains("Azure Cosmos", result);
+        }
+
+        [Fact]
+        public void TestConnectionTest()
+        {
+            var result = _instance.TestConnection(_container);
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void GetDataCollectionMetricsTest()
+        {
+            var metrics = new DataCollectionMetrics[] {
+                new DataCollectionMetrics()
+                    {Name = "EMAILS", RowCount = 200},
+                new DataCollectionMetrics()
+                    {Name = "LEADS", RowCount = 200},
+                new DataCollectionMetrics()
+                    {Name = "CONTACTS", RowCount = 200},
+            };
+
+            var result = _instance.GetDataCollectionMetrics(_container);
+            Assert.Equal(metrics.Length, result.Count);
+
+            foreach (var metric in metrics)
+            {
+                var resultMetric = result.Find(x => x.Name.Equals(metric.Name));
+                Assert.NotNull(resultMetric);
+
+                Assert.Equal(metric.RowCount, resultMetric.RowCount);
+            }
+        }
+
+        [Fact]
+        public void GetTableNamesTest()
+        {
+            var (tables, elements) = _instance.GetSchema(_container);
+            Assert.Equal(3, tables.Count);
+            Assert.Equal(103, elements.Count);
+
+            var tableNames = new string[] { "LEADS", "EMAILS", "CONTACTS" };
+            foreach (var tableName in tableNames)
+            {
+                var table = tables.Find(x => x.Name.Equals(tableName));
+                Assert.NotNull(table);
+            }
+        }
+
+        [Fact]
+        public void CollectSampleTest()
+        {
+            var entity = new DataEntity("FROM_ADDR", DataType.String, "string", _container,
+                new DataCollection(_container, "EMAILS"));
+
+            var samples = _instance.CollectSample(entity, 2);
+            Assert.Equal(2, samples.Count);
+            Assert.Contains("will@example.com", samples);
+        }
+
+
+    }
+}
